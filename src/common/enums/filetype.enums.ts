c55a15f462c2425c919c108acb1b847948dbdfae
TheNofis <matbey122@gmail.com>
2025-03-12 00:05:15 +0300

    add: filetypes

---
+++ src/common/enums/filetype.enums.ts
@@ -0,0 +1,5 @@
+export const FileType = {
+  IMAGE: ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'],
+  VIDEO: ['video/mp4', 'video/ogg', 'video/webm'],
+  AUDIO: ['audio/mpeg', 'audio/ogg', 'audio/webm'],
+};
